e1a6ae132fd877df3ecf16aae1404e4b7f1b9f5e
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-05-05 10:48:06 +0300

    1-10

--- js/task-05.js
+++
@@ -1,6 +0,0 @@
-const textInput = document.querySelector('#name-input');
-const userName = document.querySelector('#name-output')
-
-textInput.addEventListener('input', (event) => {
-    event.currentTarget.value?userName.textContent = event.currentTarget.value:userName.textContent = 'Anonymous';
-});
