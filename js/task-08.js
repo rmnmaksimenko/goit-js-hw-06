752ea90ff81ccdd9f27f292dd2eccc4b27aa3e1b
Roman Maksimenko <romanmaksimenkodev@gmail.com>
2022-06-02 14:12:17 +0300

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -5,14 +5,12 @@ const password = form.password;
 form.addEventListener('submit', submit);
 
 function submit(event) {
-    event.preventDefault();
-    if (!email.value || !password.value) {
-        alert('Все поля в форме должны быть заполнены.')
-    }
-    else
-    {
-        const formInput = { [event.currentTarget.elements[0].value]: event.currentTarget.elements[1].value, };
-        console.log(formInput);
-        event.currentTarget.reset();
-    }
+  event.preventDefault();
+  if (!email.value || !password.value) {
+    alert('Все поля в форме должны быть заполнены.');
+  } else {
+    const formInput = { email: event.currentTarget.elements[0].value, password: event.currentTarget.elements[1].value };
+    console.log(formInput);
+    event.currentTarget.reset();
+  }
 }
